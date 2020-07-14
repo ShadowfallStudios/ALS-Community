b9e54b214643e1c8079c6310f2ca3c6955eac858
Doğa Can Yanıkoğlu <dcyanikoglu@gmail.com>
2020-07-14 12:16:54 +0300

    Refresh copyright notice on source files

--- Source/ALSV4_CPP/ALSV4_CPP.h
+++ Source/ALSV4_CPP/ALSV4_CPP.h
@@ -1,4 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Project:         Advanced Locomotion System V4 on C++
+// Copyright:       Copyright (C) 2020 Doğa Can Yanıkoğlu
+// License:         MIT License (http://www.opensource.org/licenses/mit-license.php)
+// Source Code:     https://github.com/dyanikoglu/ALSV4_CPP
+// Original Author: Doğa Can Yanıkoğlu
+// Contributors:  
 
 #pragma once
 
