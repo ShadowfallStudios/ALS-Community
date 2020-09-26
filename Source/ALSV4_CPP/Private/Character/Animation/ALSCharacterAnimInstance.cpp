5cc28af7ff1cc6438f8b4bd2737f6e20d54ea700
Doğa Can Yanıkoğlu <dcyanikoglu@gmail.com>
2020-09-26 12:24:34 +0300

    Revert "Safer solution for foot IK related issues"
    This reverts commit dc34d16a2fd10be3725a7f73d9522b919c81be7b.

--- Source/ALSV4_CPP/Private/Character/Animation/ALSCharacterAnimInstance.cpp
+++ Source/ALSV4_CPP/Private/Character/Animation/ALSCharacterAnimInstance.cpp
@@ -40,10 +40,11 @@ void UALSCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 
 	UpdateAimingValues(DeltaSeconds);
 	UpdateLayerValues();
-	UpdateFootIK(DeltaSeconds);
 	
 	if (MovementState.Grounded())
 	{
+		UpdateFootIK(DeltaSeconds);
+		
 		// Check If Moving Or Not & Enable Movement Animations if IsMoving and HasMovementInput, or if the Speed is greater than 150.
 		const bool prevShouldMove = Grounded.bShouldMove;
 		Grounded.bShouldMove = ShouldMoveCheck();
@@ -257,16 +258,13 @@ void UALSCharacterAnimInstance::UpdateLayerValues()
 
 void UALSCharacterAnimInstance::UpdateFootIK(float DeltaSeconds)
 {
-	if (MovementState.Grounded())
-	{
-		// Update Foot Locking values.
-		SetFootLocking(DeltaSeconds, FName(TEXT("Enable_FootIK_L")), FName(TEXT("FootLock_L")),
-					FName(TEXT("ik_foot_l")), FootIKValues.FootLock_L_Alpha, FootIKValues.UseFootLockCurve_L,
-					FootIKValues.FootLock_L_Location, FootIKValues.FootLock_L_Rotation);
-		SetFootLocking(DeltaSeconds, FName(TEXT("Enable_FootIK_R")), FName(TEXT("FootLock_R")),
-					FName(TEXT("ik_foot_r")), FootIKValues.FootLock_R_Alpha, FootIKValues.UseFootLockCurve_R,
-					FootIKValues.FootLock_R_Location, FootIKValues.FootLock_R_Rotation);
-	}
+	// Update Foot Locking values.
+	SetFootLocking(DeltaSeconds, FName(TEXT("Enable_FootIK_L")), FName(TEXT("FootLock_L")),
+	               FName(TEXT("ik_foot_l")), FootIKValues.FootLock_L_Alpha, FootIKValues.UseFootLockCurve_L,
+	               FootIKValues.FootLock_L_Location, FootIKValues.FootLock_L_Rotation);
+	SetFootLocking(DeltaSeconds, FName(TEXT("Enable_FootIK_R")), FName(TEXT("FootLock_R")),
+	               FName(TEXT("ik_foot_r")), FootIKValues.FootLock_R_Alpha, FootIKValues.UseFootLockCurve_R,
+	               FootIKValues.FootLock_R_Location, FootIKValues.FootLock_R_Rotation);
 
 	if (MovementState.InAir())
 	{
@@ -274,7 +272,7 @@ void UALSCharacterAnimInstance::UpdateFootIK(float DeltaSeconds)
 		SetPelvisIKOffset(DeltaSeconds, FVector::ZeroVector, FVector::ZeroVector);
 		ResetIKOffsets(DeltaSeconds);
 	}
-	else if (MovementState.Grounded())
+	else if (!MovementState.Ragdoll())
 	{
 		// Update all Foot Lock and Foot Offset values when not In Air
 		FVector FootOffsetLTarget;
