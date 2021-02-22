2a5999874fd4112a3e2acdc5fa57bed06d14f41b
Doğa Can Yanıkoğlu <dcyanikoglu@gmail.com>
2021-02-22 15:34:37 +0300

    Fix debug component crash issues in clients

--- Source/ALSV4_CPP/Private/Character/ALSPlayerController.cpp
+++ Source/ALSV4_CPP/Private/Character/ALSPlayerController.cpp
@@ -14,50 +14,61 @@
 
 void AALSPlayerController::OnPossess(APawn* NewPawn)
 {
-	PossessedCharacter = Cast<AALSBaseCharacter>(NewPawn);
 	Super::OnPossess(NewPawn);
-
-	// Servers want to setup camera only in listen servers.
+	PossessedCharacter = Cast<AALSBaseCharacter>(NewPawn);
 	if (!IsRunningDedicatedServer())
 	{
+		// Servers want to setup camera only in listen servers.
 		SetupCamera();
 	}
-
-	// Bind inputs for debugging
-	UActorComponent* Comp = NewPawn->GetComponentByClass(UALSDebugComponent::StaticClass());
-	if (Comp)
-	{
-		UALSDebugComponent* DebugComp = Cast<UALSDebugComponent>(Comp);
-		InputComponent->BindKey(FKey("Tab"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleHud);
-		InputComponent->BindKey(FKey("V"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleDebugView);
-		InputComponent->BindKey(FKey("T"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleTraces);
-		InputComponent->BindKey(FKey("Y"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleDebugShapes);
-		InputComponent->BindKey(FKey("U"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleLayerColors);
-		InputComponent->BindKey(FKey("I"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleCharacterInfo);
-		InputComponent->BindKey(FKey("Z"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleSlomo);
-	}
+	SetupDebugInputs();
 }
 
 void AALSPlayerController::OnRep_Pawn()
 {
 	Super::OnRep_Pawn();
+	PossessedCharacter = Cast<AALSBaseCharacter>(GetPawn());
 	SetupCamera();
+}
+
+void AALSPlayerController::BeginPlayingState()
+{
+	Super::BeginPlayingState();
+	if (GetLocalRole() == ROLE_AutonomousProxy)
+	{
+		SetupDebugInputs();
+	}
 }
 
 void AALSPlayerController::SetupCamera()
 {
-	PossessedCharacter = Cast<AALSBaseCharacter>(GetPawn());
-	check(PossessedCharacter);
-
 	// Call "OnPossess" in Player Camera Manager when possessing a pawn
 	AALSPlayerCameraManager* CastedMgr = Cast<AALSPlayerCameraManager>(PlayerCameraManager);
-	if (CastedMgr)
+	if (PossessedCharacter && CastedMgr)
 	{
 		CastedMgr->OnPossess(PossessedCharacter);
 	}
 }
 
-void AALSPlayerController::SetupInputComponent()
+void AALSPlayerController::SetupDebugInputs()
 {
-	Super::SetupInputComponent();
+	// Bind inputs for debugging
+	if (PossessedCharacter)
+	{
+		UActorComponent* Comp = PossessedCharacter->GetComponentByClass(UALSDebugComponent::StaticClass());
+		if (Comp)
+		{
+			UALSDebugComponent* DebugComp = Cast<UALSDebugComponent>(Comp);
+			if (InputComponent && DebugComp)
+			{
+				InputComponent->BindKey(FKey("Tab"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleHud);
+				InputComponent->BindKey(FKey("V"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleDebugView);
+				InputComponent->BindKey(FKey("T"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleTraces);
+				InputComponent->BindKey(FKey("Y"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleDebugShapes);
+				InputComponent->BindKey(FKey("U"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleLayerColors);
+				InputComponent->BindKey(FKey("I"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleCharacterInfo);
+				InputComponent->BindKey(FKey("Z"), EInputEvent::IE_Pressed, DebugComp, &UALSDebugComponent::ToggleSlomo);
+			}
+		}
+	}
 }
